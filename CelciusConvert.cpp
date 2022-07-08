469bf0d332632c83c6af3d9044c1e865fc0d3e1e
Greenteapocky029 <62956955+Greenteapocky029@users.noreply.github.com>
2022-07-08 18:13:10 -0400

    Update CelciusConvert.cpp

--- CelciusConvert.cpp
+++ CelciusConvert.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 //Name: Deven Greenlee 
 //modified: 6/27/2022
